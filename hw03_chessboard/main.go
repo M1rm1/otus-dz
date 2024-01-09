18aab2f5d3584b62cb3de414f7cbb40602b10e5d
Alexandr Pipko <a_pipko@mail.ru>
2024-01-09 12:28:58 +0300

    HW03 done?

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	var size int
+
+	fmt.Scanf("%d", &size)
+
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			if i%2 == 0 && j%2 == 0 || i%2 != 0 && j%2 != 0 {
+				fmt.Printf("#")
+			} else {
+				fmt.Print(" ")
+			}
+			if j == size-1 {
+				fmt.Printf("\n")
+			}
+		}
+	}
 }
